11ebf21866f60e46c238c9d800643307abd4955a
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-12-17 00:07:46 +0100

    Creat Props

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+interface CardI {
+    character: string;
+    imgSrc: string;
+}
+
+function Card ({character,imgSrc}: CardI )
+ { return (
+    <figure>
+
+    <img src= {imgSrc} alt={`Representatation of ${character}`} />
+
+    <figcaption>{character} </figcaption>
+</figure>
+
+);
+}
+
+   
+  
+ 
+
+
+export default Card;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,50 +1,35 @@
+import Card from "../Card";
 import "./Home.css"
 
+
+
 function Home() {
+
+    const data = [
+        {
+            Character: "Mario",
+            imgSrc: "https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png"
+        },
+        {
+            Character: "Peach",
+            imgSrc: "https://upload.wikimedia.org/wikipedia/en/thumb/1/16/Princess_Peach_Stock_Art.png/170px-Princess_Peach_Stock_Art.png"
+        }
+
+    ];
+
+    
 
     return (
 
        <section className="card-section">
-        <figure>
+        
+        < Card character={data[0].Character} imgSrc={data[0].imgSrc}/>
+        < Card character={data[1].Character} imgSrc={data[1].imgSrc} />
+        
 
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="representation de mario" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-        <figure>
-
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-        <figure>
-
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-
-<figure>
-
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="representation de mario" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-        <figure>
-
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-        <figure>
-
-            <img src="https://upload.wikimedia.org/wikipedia/en/5/5c/Mario_by_Shigehisa_Nakaue.png" alt="" />
-
-            <figcaption>Mario</figcaption>
-        </figure>
-
-       </section>
-    )
+        </section>
+    );
 }
+
 
 export default Home;
