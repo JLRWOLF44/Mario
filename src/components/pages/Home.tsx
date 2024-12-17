315b6f1b33b8925c922514a0105b151b6ceff470
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-12-17 16:13:19 +0100

    Creat state

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,25 +1,29 @@
 import Card from "../Card";
-import "./Home.css"
+import "./Home.css";
 import data from "../../assets/data";
-
+import { useState } from "react";
 
 function Home() {
+  const [count, setCount] = useState(0);
 
-   
-    
+  function handleCount() {
+    setCount(count + 1);
+  }
 
-    return (
-
-       <section className="card-section">
-        
-        < Card character={data[0].Character} imgSrc={data[0].imgSrc}/>
-        < Card character={data[1].Character} imgSrc={data[1].imgSrc} />
-        < Card character={data[2].Character} imgSrc={data[2].imgSrc} />
-        
-
-        </section>
-    );
+  return (
+    <>
+      <section className="card-section">
+        {data.map((el) => {
+          return (
+            <Card key={el.imgSrc} character={el.Character} imgSrc={el.imgSrc} />
+          );
+        })}
+      </section>
+      <button onClick={handleCount} id="count">
+        Mon Score : {count}{" "}
+      </button>
+    </>
+  );
 }
-
 
 export default Home;
