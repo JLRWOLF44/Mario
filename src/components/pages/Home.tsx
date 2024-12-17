4e059d57c5174d6d0db90fb49ff31819ec1e68c9
JLRWOLF44 <jeanlouisroger44@gmail.com>
2024-12-17 23:40:54 +0100

    re-invoice and method maps

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,24 +4,29 @@ import data from "../../assets/data";
 import { useState } from "react";
 
 function Home() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("...");
 
-  function handleCount() {
-    setCount(count + 1);
+  function handleSelect(event: React.ChangeEvent<HTMLSelectElement>) {
+    setValue(event.target.value);
   }
-
+  52;
+  const filterdArray =
+    value !== "..." ? data.filter((el) => el.type.includes(value)) : data;
   return (
     <>
+      <select onChange={handleSelect}>
+        <option>...</option>
+        <option>Good</option>
+        <option>Evil</option>
+      </select>
+
       <section className="card-section">
-        {data.map((el) => {
+        {filterdArray.map((el) => {
           return (
             <Card key={el.imgSrc} character={el.Character} imgSrc={el.imgSrc} />
           );
         })}
       </section>
-      <button onClick={handleCount} id="count">
-        Mon Score : {count}{" "}
-      </button>
     </>
   );
 }
